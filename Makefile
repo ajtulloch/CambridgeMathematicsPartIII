27fcd19b5e4d188613c3a02b299d5a6e56e0c62f
Andrew Tulloch <andrew@tullo.ch>
2014-10-26 03:24:17 -0700

    Adding PDFS

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ DBOX=~/Dropbox/Sharing/CambridgeSummaries
 
 summaries:
 	find . -iname 'master.pdf' | xargs -I{} gcp --parents {} $(DBOX)
+
+collate:
+	mkdir -p scratch/
+	find . -iname 'master.pdf' | xargs -I{} cp $(echo "{}" | tr / -) scratch/
---
+++ collate.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -x
+set -e
+
+mkdir -p /tmp/scratch
+rm -rf /tmp/scratch/*
+
+find $(pwd) -iname 'master.tex' -print0 | xargs -0 sh -c 'for f; do echo "$f"; cd $(dirname $f) && latexmk -C $f && latexmk -pdf $f; done'
+
+for f in $(find . -iname 'master.pdf'); do
+    cp $f /tmp/scratch/$(echo "${f:2}" | tr / - | sed -e 's/-master//')
+done
+
